9567c82a69b6cfb27a6e78f0f6b315acbbce5b51
Shahnawaz Ansari <110127259+Shahnawazansari23@users.noreply.github.com>
2022-11-10 22:02:34 +0530

    Add files via upload
    Digital clock by using html,css and javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var section = document.getElementById('session');
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+    
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
